73d10d93efd5cb067805485f14321fcda2a3233f
CyberRauber <cyyberRauber@gmail.com>
2026-07-22 14:31:35 -0300

    fe

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 class Raca:
-    def __init__(self, nome, forca, vida, velocidade, inteligencia):
+    def __init__(self, nome, forca, vida, velocidade, inteligencia, resistencia):
         self.nome = nome
         self.forca = forca
         self.vida = vida
         self.velocidade = velocidade
         self.inteligencia = inteligencia
+        self.resistencia = resistencia
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import classes
+import random
 
-rHuman = classes.Raca("Humano", 10, 100, 10, 10)
-rAlien = classes.Raca("Alien", 15, 150, 15, 15)
-rHibrido = classes.Raca("Híbrido", 20, 200, 20, 20)
+rHuman = classes.Raca("Humano", 10, 100, 15, 10, 10) #nome, força, vida, velocidade, inteligência, resistência
+
+rAlien = classes.Raca("Alien", 15, 150, 15, 15, 15)
+
+rHibrido = classes.Raca("Híbrido", 20, 200, 20, 20, 20)
+
+rZumbi = classes.Raca("Zumbi", 25, 250, 10, 25, 25)
 
 
 def cad(): #Cadastro de usuário 
     nome = input("Insira o nome do usuário: ")
-
+    r = input("""Escolha a raça do usuário: 
+    [1] Humano
+    [2] Alien
+    [3] Híbrido
+    [4] Zumbi
+    """"")
