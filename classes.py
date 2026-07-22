d93ca5c5232b5298ccfc0db2f2048c0b7d3df99b
CyberRauber <cyyberRauber@gmail.com>
2026-07-22 14:10:47 -0300

    fe

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Raca:
+    def __init__(self, nome, forca, vida, velocidade, inteligencia):
+        self.nome = nome
+        self.forca = forca
+        self.vida = vida
+        self.velocidade = velocidade
+        self.inteligencia = inteligencia
---
+++ main.py
@@ -0,0 +1,10 @@
+import classes
+
+rHuman = classes.Raca("Humano", 10, 100, 10, 10)
+rAlien = classes.Raca("Alien", 15, 150, 15, 15)
+rHibrido = classes.Raca("Híbrido", 20, 200, 20, 20)
+
+
+def cad(): #Cadastro de usuário 
+    nome = input("Insira o nome do usuário: ")
+
