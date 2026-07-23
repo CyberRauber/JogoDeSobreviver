868e833803fa39b7c3200c50e4927e36dbb2fedd
CyberRauber <cyyberRauber@gmail.com>
2026-07-23 15:40:09 -0300

    Fe

--- classes.py
+++ classes.py
@@ -1,9 +1,26 @@
-class Raca:
-    def __init__(self, nome, forca, vida, velocidade, inteligencia, resistencia, magia=0):
+import random
+
+forca = random.randint(10, 20)
+vida = random.randint(90, 115)
+velocidade = random.randint(7, 15)
+inteligencia = random.randint(270, 330)
+resistencia = random.randint(50, 70)
+
+class Personagem:
+    def __init__(self, nome, vida=vida, forca=forca, velocidade=velocidade, inteligencia=inteligencia, resistencia=resistencia, inventario=None):
         self.nome = nome
-        self.forca = forca
         self.vida = vida
+        self.forca = forca
         self.velocidade = velocidade
         self.inteligencia = inteligencia
-        self.magia = magia
         self.resistencia = resistencia
+        self.inventario = inventario
+
+class Item:
+    def __init__(self, nome, tipo, dano, efeito, quantidade=0):
+        self.nome = nome
+        self.tipo = tipo
+        self.dano = dano
+        self.quantidade = quantidade
+        self.efeito = efeito
+
--- main.py
+++ main.py
@@ -1,39 +1,24 @@
 import classes
-import random
 
-rHuman = classes.Raca("Humano", 10, 100, 15, 10, 10, 0) #nome, força, vida, velocidade, inteligência, resistência
+p = 25
 
-rAlien = classes.Raca("Alien", 15, 150, 15, 15, 15)
+def cadastro(mA):
+    nome = input("Cadastro do Usuário \n Digite o nome: ")
+    print(f"Bem-vindo(a), {nome}! \n seus atributos iniciais são: ")
+    print(mA)
 
-rHibrido = classes.Raca("Híbrido", 20, 200, 20, 20, 20)
+def AddP(p):
+    pass
 
-rZumbi = classes.Raca("Zumbi", 25, 250, 10, 25, 25)
+def mA():
+    return f"""
+        Vida: {classes.vida}
+        Força: {classes.forca}
+        Velocidade: {classes.velocidade}
+        Inteligência: {classes.inteligencia}
+        Resistência: {classes.resistencia}
+"""
 
-rMago = classes.Raca("Mago", 5, 50, 10, 30, 5, 50) #nome, força, vida, velocidade, inteligência, resistência, magia
+mA = mA()
 
-def cad(): #Cadastro de usuário 
-    nome = input("Insira o nome do usuário: ")
-    rOpc = input("""Escolha a raça do usuário: 
-    [1] Humano
-    [2] Alien
-    [3] Híbrido
-    [4] Zumbi
-    [5] Mago
-    """"")
-    match rOpc:
-        case "1":
-            raca = rHuman
-            print(f"{nome} \n Força: {raca.forca}\n Vida: {raca.vida} \n Velocidade: {raca.velocidade} \n Inteligência: {raca.inteligencia} \n Resistência: {raca.resistencia}")
-        case "2":
-            raca = rAlien
-            print(f"{nome} \n Força: {raca.forca}\n Vida: {raca.vida} \n Velocidade: {raca.velocidade} \n Inteligência: {raca.inteligencia} \n Resistência: {raca.resistencia}")
-        case "3":
-            raca = rHibrido
-            print(f"{nome} \n Força: {raca.forca}\n Vida: {raca.vida} \n Velocidade: {raca.velocidade} \n Inteligência: {raca.inteligencia} \n Resistência: {raca.resistencia}")
-        case "4":
-            raca = rZumbi
-            print(f"{nome} \n Força: {raca.forca}\n Vida: {raca.vida} \n Velocidade: {raca.velocidade} \n Inteligência: {raca.inteligencia} \n Resistência: {raca.resistencia}")
-        case "5":
-            raca = rMago
-            print(f"{nome} \n Força: {raca.forca}\n Vida: {raca.vida} \n Velocidade: {raca.velocidade} \n Inteligência: {raca.inteligencia} \n Resistência: {raca.resistencia}")    
-cad()
+cadastro(mA)
